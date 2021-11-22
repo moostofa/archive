90d1ab87c925a68a43628976f44e7fcd234846ea
moostofa <mmdrahinz96@gmail.com>
2021-11-22 21:59:38 +1100

    created watchlist model

--- books/models.py
+++ books/models.py
@@ -1,4 +1,13 @@
 from django.contrib.auth.models import AbstractUser
+from django.db.models import Model, ForeignKey, CASCADE
+from django.db.models.fields import TextField
 
 class User(AbstractUser):
     pass
+
+class Watchlist(Model):
+    user = ForeignKey(
+        User,
+        on_delete=CASCADE
+    )
+    books = TextField()
