4c0f16d3817061911ad25169cf108f93fad90024
moostofa <mmdrahinz96@gmail.com>
2021-11-24 14:28:41 +1100

    accept and validate registration form in views.py /register

--- books/views.py
+++ books/views.py
@@ -1,9 +1,12 @@
+from django.forms import Form
 from django.forms.fields import CharField
 from django.forms.widgets import EmailInput, PasswordInput, TextInput
+from django.http import JsonResponse
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from django.http import JsonResponse
-from django.forms import Form
+
+from .models import User
+
 
 # login form to display to the user
 class LoginForm(Form):
@@ -15,7 +18,7 @@ class LoginForm(Form):
     )
 
 
-# register form to display to use - inherits from LoginForm + an email field
+# register form to display to use - inherits fields from LoginForm
 class RegisterForm(LoginForm):
     email = CharField(label="", 
         widget=EmailInput(attrs={"placeholder": "Email"})
@@ -28,6 +31,38 @@ class RegisterForm(LoginForm):
 # display index page
 def index(request):
     return render(request, "books/index.html")
+
+
+# register user and validate credentials
+def register(request):
+    if request.method == "GET":
+        return render(request, "books/index.html", {
+            "register": True,
+            "register_form": RegisterForm()
+        })
+    else:
+        form = RegisterForm(request.POST)
+        if not form.is_valid():
+            return HttpResponse("Error in register function() - register form is invalid")
+        
+        # retreive form fields
+        username = form.cleaned_data["username"]
+        email = form.cleaned_data["email"]
+        password = form.cleaned_data["password"]
+        confirm_password = form.cleaned_data["confirm_password"]
+        
+        # validate and confirm password match
+        # if passwords do not match, re-promt user to type in matching passwords
+        if (password != confirm_password):
+            return render(request, "books/index.html", {
+                "register": True,
+                "message": "Invalid credentials: passwords do not match",
+                "register_form": RegisterForm(initial={
+                    "username": username,
+                    "email": email
+                })
+            })
+        return HttpResponse("TODO: user can be registered")
 
 
 # allow user to login
@@ -45,17 +80,6 @@ def login_view(request):
 # (btw, on google, saw something like configuring LOGIN_REDIRECT_URL and LOGOUT_REDIRECT_URL in settings.py)
 def logout_view(request):
     pass
-
-
-# register user and validate credentials
-def register(request):
-    if request.method == "GET":
-        return render(request, "books/index.html", {
-            "register": True,
-            "login_form": RegisterForm()
-        })
-    else:
-        return HttpResponse("TODO: Register user")
 
 # testing fetch() from script.js
 def action(request):
