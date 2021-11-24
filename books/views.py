bc171f991972ea8a5eef5bc0803e3a4062e110e0
moostofa <mmdrahinz96@gmail.com>
2021-11-24 15:05:29 +1100

    completed user authentication & created some test users

--- books/views.py
+++ books/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
 from django.forms import Form
 from django.forms.fields import CharField
 from django.forms.widgets import EmailInput, PasswordInput, TextInput
 from django.http import JsonResponse
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from .models import User
 
@@ -18,7 +21,7 @@ class LoginForm(Form):
     )
 
 
-# register form to display to use - inherits fields from LoginForm
+# register form to display to the user - inherits fields from LoginForm
 class RegisterForm(LoginForm):
     email = CharField(label="", 
         widget=EmailInput(attrs={"placeholder": "Email"})
@@ -33,14 +36,16 @@ def index(request):
     return render(request, "books/index.html")
 
 
-# register user and validate credentials
+# register user and validate their credentials
 def register(request):
     if request.method == "GET":
+        # render a registration form
         return render(request, "books/index.html", {
             "register": True,
             "register_form": RegisterForm()
         })
     else:
+        # retreive and validate registration form
         form = RegisterForm(request.POST)
         if not form.is_valid():
             return HttpResponse("Error in register function() - register form is invalid")
@@ -62,24 +67,57 @@ def register(request):
                     "email": email
                 })
             })
-        return HttpResponse("TODO: user can be registered")
+        # try to create a new user
+        # IntegrityError is raised if username is already taken
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "books/index.html", {
+                "register": True,
+                "message": "Invalid credentials: username is already taken",
+                "register_form": RegisterForm(initial={
+                    "email": email
+                })
+            })
+        # log user in and redirect to index
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
 
 
 # allow user to login
 def login_view(request):
     if request.method == "GET":
+        # render a login form
         return render(request, "books/index.html", {
             "login": True,
             "login_form": LoginForm()
         })
     else:
-        return HttpResponse("TODO: Login user")
+        # get user's credentials and authenticate them
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username = username, password = password)
+
+        # if user is not registered
+        if not user:
+            return render(request, "books/index.html", {
+                "login": True,
+                "message": "Invalid credentials: username and/or password are incorrect",
+                "login_form": LoginForm(initial={
+                    "username": username
+                })
+            })
+        # log user in and redirect to index
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
 
 
 # log user out 
 # (btw, on google, saw something like configuring LOGIN_REDIRECT_URL and LOGOUT_REDIRECT_URL in settings.py)
 def logout_view(request):
-    pass
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
 
 # testing fetch() from script.js
 def action(request):
