2028aad99a3d43392cbcec72aed6cb1475041ee6
moostofa <mmdrahinz96@gmail.com>
2021-11-26 21:22:29 +1100

    separated column creation into functions

--- books/static/books/script.js
+++ books/static/books/script.js
@@ -43,80 +43,13 @@ function search() {
                 // main row - will consist of a col-3 for image, a col-6 for book info, a col-3 for other buttons
                 const row = document.createElement("div")
                 row.classList = "row"
-        
-                // 1st col to display cover image
-                const imgCol = document.createElement("div")
-                imgCol.classList = "col-3"
-    
-                // fetch cover img for book
-                fetch(`https://covers.openlibrary.org/b/OLID/${bookId}-L.jpg`)
-                .then(response => response.blob())
-                .then(blob => {
-                    const img = document.createElement("img")
-                    img.src = URL.createObjectURL(blob)
-    
-                    // if natural height/width is 1, then there is no cover img
-                    img.onload = () => {
-                        if (img.naturalHeight === 1)
-                            imgCol.style.backgroundColor = "grey"
-                    } 
-                    imgCol.appendChild(img)
-                })
-                .catch(error => console.log(`Error in search() function - cover image fetch() - failed to fetch cover image from openlibrary API - ${error}`))
-    
-                // 2nd col to display info about the book
-                const infoCol = document.createElement("div")
-                infoCol.classList = "col-6"
-        
-                // get the specific FIELDS of the book and save them as a list
-                const ul = document.createElement("ul")
-                Object.entries(FIELDS).forEach(([key, value]) => {
-                    let fieldValue = book[value]
-    
-                    // try limiting genres list to 5: a TypeError is raised if .slice() is used on an empty array of genres
-                    if (key === "Genres")
-                        try {
-                            fieldValue = fieldValue.slice(0, 5)
-                        } catch(error) {
-                            fieldValue = "None"
-                        }
-                    ul.innerHTML += `<li><strong>${key}:</strong> ${fieldValue}</li>`
-                })
-                infoCol.appendChild(ul)
-    
-                // 3rd col will tell user if the book is in their reading list, or, display a select menu of reading lists they can add to
-                const actionCol = document.createElement("div")
-                actionCol.classList = "col-3"
 
-                // find if the current book is in the user's reading list, and which specific list
-                let bookIdInReadingList = false
-                let whichReadingList = ""
-                for (let [key, value] of Object.entries(readingList)) {
-                    if (value.includes(bookId)) {   // values are arrays
-                        bookIdInReadingList = true
-                        whichReadingList = key
-                        break
-                    }
-                }
-                // if book is in the user's reading list, then say so, 
-                // else, create a select menu so user can add the book to their reading list
-                if (bookIdInReadingList) {
-                    actionCol.innerHTML += `<button>Book in ${whichReadingList} list</button>`
-                } else {
-                    const selectMenu = document.createElement("select")
-                    selectMenu.innerHTML += `<option selected disabled>Add to my list</option>`
+                const imgCol = getCoverImg(bookId)
         
-                    // create options for user to add book to any of their reading lists
-                    Object.keys(readingList).forEach(element => {
-                        const option = document.createElement("option")
-                        option.value = element
-                        option.innerHTML = element[0].toUpperCase() + element.substring(1)
-                        selectMenu.appendChild(option)
-                    })
-                    // listen for an option select and add the book to that user's chosen list
-                    selectMenu.addEventListener("change", () => addToReadingList(bookId, selectMenu.value))
-                    actionCol.appendChild(selectMenu)
-                }
+                const infoCol = getDetails(book)
+
+                const actionCol = displayArchiveOptions(bookId, readingList)
+    
                 // append columns to the row & add the row to the container
                 row.append(imgCol, infoCol, actionCol)
                 document.getElementById("book-results").appendChild(row)
@@ -126,6 +59,92 @@ function search() {
     })
     .catch(error => console.log(`Error in search() function - outer fetch() - failed to fetch user's reading list from /mylist route - ${error}`))
 
+}
+
+// objectId can be bookId, movieId, animeId, or mangaId
+function getCoverImg(objectId) {
+    const imgCol = document.createElement("div")
+    imgCol.classList = "col-3"
+
+    // fetch cover img for book
+    fetch(`https://covers.openlibrary.org/b/OLID/${objectId}-L.jpg`)
+    .then(response => response.blob())
+    .then(blob => {
+        const img = document.createElement("img")
+        img.src = URL.createObjectURL(blob)
+
+        // if natural height/width is 1, then there is no cover img
+        img.onload = () => {
+            if (img.naturalHeight === 1)
+                imgCol.style.backgroundColor = "grey"
+        } 
+        imgCol.appendChild(img)
+    })
+    .catch(error => console.log(`Failed to fetch cover image from openlibrary API - ${error}`))
+    return imgCol
+}
+
+// object can be a book, movie, anime, or manga
+function getDetails(object) {
+    // 2nd col to display info about the object
+    const infoCol = document.createElement("div")
+    infoCol.classList = "col-6"
+
+    // get the specific FIELDS of the object and save them as a list
+    const ul = document.createElement("ul")
+    Object.entries(FIELDS).forEach(([key, value]) => {
+        let fieldValue = object[value]
+
+        // try limiting genres list to 5: a TypeError is raised if .slice() is used on an empty array of genres
+        if (key === "Genres")
+            try {
+                fieldValue = fieldValue.slice(0, 5)
+            } catch(error) {
+                fieldValue = "None"
+            }
+        ul.innerHTML += `<li><strong>${key}:</strong> ${fieldValue}</li>`
+    })
+    infoCol.appendChild(ul)
+    return infoCol
+}
+
+// objectId can be bookId, movieId, animeId, or mangaId
+// usersList can be the user's readingList, movieList, animeList, or mangaList
+function displayArchiveOptions(objectId, usersList) {
+    // 3rd col will tell user if the book is in their reading list, or, display a select menu of reading lists they can add to
+    const actionCol = document.createElement("div")
+    actionCol.classList = "col-3"
+
+    // find if the current book is in the user's reading list, and which specific list
+    let bookIdInReadingList = false
+    let whichReadingList = ""
+    for (let [key, value] of Object.entries(usersList)) {
+        if (value.includes(objectId)) {   // values are arrays
+            bookIdInReadingList = true
+            whichReadingList = key
+            break
+        }
+    }
+    // if book is in the user's reading list, then say so, 
+    // else, create a select menu so user can add the book to their reading list
+    if (bookIdInReadingList) {
+        actionCol.innerHTML += `<button>Book in ${whichReadingList} list</button>`
+    } else {
+        const selectMenu = document.createElement("select")
+        selectMenu.innerHTML += `<option selected disabled>Add to my list</option>`
+
+        // create options for user to add book to any of their reading lists
+        Object.keys(usersList).forEach(element => {
+            const option = document.createElement("option")
+            option.value = element
+            option.innerHTML = element[0].toUpperCase() + element.substring(1)
+            selectMenu.appendChild(option)
+        })
+        // listen for an option select and add the book to that user's chosen list
+        selectMenu.addEventListener("change", () => addToReadingList(objectId, selectMenu.value))
+        actionCol.appendChild(selectMenu)
+    }
+    return actionCol
 }
 
 // send POST data to /add in views.py to add book to user's reading list
